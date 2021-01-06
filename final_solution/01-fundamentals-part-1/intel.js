fa0330f4b07f66652b81a25597a7e0292877e955
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2021-01-06 13:08:11 -0500

    Comparison

--- final_solution/01-fundamentals-part-1/intel.js
+++ final_solution/01-fundamentals-part-1/intel.js
@@ -35,6 +35,18 @@ console.log(x);
 //  /// //////////////////// //////
 // /// Comparison Operator //////////
 //  /// //////////////////// //////
+//  > , < , >= , ==
+
+console.log(ageJonas > ageSarah); // asking the operator who's older true or false
+console.log(ageSarah >= 18); // true she is older than 18
+
+const isFullAge = ageSarah >= 18;
+console.log (now - 1991 > now - 2018);
+
+
+
+
+
 
 //  /////// /////////////// ///////////////////////////
 //   /////// 3 different ways to declare variables/////

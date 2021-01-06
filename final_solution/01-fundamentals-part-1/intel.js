b604ebb98f10d7383549aaf5f07aeb223b05ede4
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2021-01-05 19:02:51 -0500

    more values

--- final_solution/01-fundamentals-part-1/intel.js
+++ final_solution/01-fundamentals-part-1/intel.js
@@ -2,10 +2,35 @@ let js = "amazing";
 // if(js === "amazing") alert("javascript is FUN playboy!");
 console.log(40 + 8 + 23 - 10);
 
-console.log("Jonas");
+console.log("jonas");
 console.log(23)
 
-let firstName = "ego";
+console.log("======================");
+
+let firstName = "Matilda";
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
+
+console.log("======================");
+
+
+// variable name conventions
+let jonas_matilda = "jm";
+let $function = 27;
+
+console.log(jonas_matilda);
+console.log($function);
+
+console.log("======================");
+
+let myFirstjob = "Mechanic";
+let currentjob = "Student";
+
+console.log(myFirstjob);
+console.log(currentjob);
+
+console.log("======================");
+
+let job1 = "Mechanic";
+let job2 = "Student";

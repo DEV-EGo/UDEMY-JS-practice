334ae82680e02d0f507877464f39b39ae3ed5da0
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2021-01-06 22:11:25 -0500

    backticks

--- final_solution/01-fundamentals-part-1/template_literals.js
+++ final_solution/01-fundamentals-part-1/template_literals.js
@@ -10,5 +10,16 @@ const year = 2037
 const jonas = " I'm " + firstName + " , a " + (year - birthYear) + " years old " + job + "!";
 console.log(jonas); 
 
-const jonasNew = `I'm ${firstName}`;
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
 console.log(jonasNew);
+
+console.log(`just a regular string...`);
+
+console.log(`string with \n\
+multiple \n\
+lines`);
+
+// using (``) backticks to create multiline html elements to insert into a page dynamically
+console.log(`string
+multiple
+lines`);

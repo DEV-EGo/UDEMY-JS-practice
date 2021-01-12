5a9a603a11bbf64974c18df4b679cc310660017e
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2021-01-12 12:53:35 -0500

    more practice

--- final_solution/01-fundamentals-part-1/license_Check.js
+++ final_solution/01-fundamentals-part-1/license_Check.js
@@ -9,23 +9,23 @@
 //     console.log(" Sarah can drive 🖕🏽");
 // };
 
-const age = 15;
-if (age >= 18){
-    console.log("Sarah can drive 🏎");
-}else {
-    const yearsLeft = 18 - age;
-    console.log(`sarach is to young . wait another ${yearsLeft} years.` );
-};
+// const age = 15;
+// if (age >= 18){
+//     console.log("Sarah can drive 🏎");
+// }else {
+//     const yearsLeft = 18 - age;
+//     console.log(`sarach is to young . wait another ${yearsLeft} years.` );
+// };
 
-const birthYear = 1998;
+// const birthYear = 1998;
 
-let century;
-if(birthYear <= 2000){
- century = 20;
-}else{
-century = 21;
-}
-console.log(century);
+// let century;
+// if(birthYear <= 2000){
+//  century = 20;
+// }else{
+// century = 21;
+// }
+// console.log(century);
  
 
 const massMark = 78;
@@ -47,4 +47,20 @@ if(BMIMark >BMIJohn) {
     console.log(`mark bmi (${BMIMark})is higher than john's!(${BMIJohn}) `)
 }else{
     console.log(`john bmi (${BMIMark}) is higher than mark's! (${BMIJohn})`)
+}
+
+const massEgo = 74;
+const heightEgo = 2.69;
+const massGracie = 44;
+const heightGracie = .95;
+
+const BMIEgo = massEgo / heightEgo **2;
+const BMIGracie = massGracie / (heightGracie * heightGracie);
+const EdwinHigherBMI = BMIEgo / BMIGracie;
+
+console.log(BMIEgo , BMIEgo);
+if( BMIEgo > BMIGracie){
+    console.log( `ego bmi (${BMIEgo}) is higher than gracies! (${BMIGracie})`);
+}else{
+    console.log(` gracie bmi (${BMIEgo}) is higher than edwin (${BMIGracie})`)
 }

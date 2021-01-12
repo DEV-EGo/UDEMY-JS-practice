b823d2d880a78223acee73f88f1caa5fc1145e00
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2021-01-12 13:06:22 -0500

    strings and numbers

---
+++ final_solution/01-fundamentals-part-1/conversion.js
@@ -0,0 +1,11 @@
+        //////// type conversion and coercion practice ////
+//  CONVERSION is when we manually convert from one type to another
+// COERCION is when javascript automatically converts types behind the scene for you
+
+const YearInput = `1991`;
+
+// *** using the built in Number function to convert strings to numbers
+// *** inside the console log numbers are pink and strings are in white
+console.log(Number(YearInput));
+console.log(YearInput + 18);
+

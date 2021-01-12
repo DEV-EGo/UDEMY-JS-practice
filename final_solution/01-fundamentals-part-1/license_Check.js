91691a456f4b35b26f6fe6f37f5253a448a95b6c
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2021-01-12 12:45:45 -0500

    add more work

--- final_solution/01-fundamentals-part-1/license_Check.js
+++ final_solution/01-fundamentals-part-1/license_Check.js
@@ -27,10 +27,16 @@ century = 21;
 }
 console.log(century);
  
-const massMark = 95;
-const heightMark = 1.88;
-const massJohn = 85;
-const heightJohn = 1.76
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95; 
+
+// const massMark = 95;
+// const heightMark = 1.88;
+// const massJohn = 85;
+// const heightJohn = 1.76
 
 const BMIMark = massMark / heightMark **2;
 const BMIJohn = massJohn / (heightJohn * heightJohn);
@@ -38,7 +44,7 @@ const markHigherBMI = BMIMark > BMIJohn;
 
 console.log(BMIJohn, BMIJohn);
 if(BMIMark >BMIJohn) {
-    console.log("mark bmi is higher than john's! ")
+    console.log(`mark bmi (${BMIMark})is higher than john's!(${BMIJohn}) `)
 }else{
-    console.log("john bmi is higher than mark's! ")
+    console.log(`john bmi (${BMIMark}) is higher than mark's! (${BMIJohn})`)
 }

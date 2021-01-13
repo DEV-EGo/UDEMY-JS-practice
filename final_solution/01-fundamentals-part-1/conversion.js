f594bdeed9dd5158555cdf3b58dc24ee27baba3c
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2021-01-12 21:05:02 -0500

    coercion

--- final_solution/01-fundamentals-part-1/conversion.js
+++ final_solution/01-fundamentals-part-1/conversion.js
@@ -2,6 +2,9 @@
 //  CONVERSION is when we manually convert from one type to another
 // COERCION is when javascript automatically converts types behind the scene for you
 
+
+// Type Conversion ..........
+
 const YearInput = `1991`;
 
 // *** using the built in Number function to convert strings to numbers
@@ -15,3 +18,10 @@ console.log(Number(`Jonas`));
 console.log(typeof NaN);
 
 console.log(String(23), 23);
+
+// Type Coercion
+
+console.log(`I am` + 23 + ` Years old`);
+console.log(`23` - `10` - 3);
+console.log(`23` / `2`);
+console.log(`23` > `18`);
